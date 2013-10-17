917ca53c86e30d634c918c7d9b0565384d12d016
Elina <elina.sundukova@gmail.com>
2013-10-17 18:31:40 +0200

    AVM Wissendatenbank seite

--- Klickdummy/assets/js/app.js
+++ Klickdummy/assets/js/app.js
@@ -1,7 +1,24 @@
-$('document').ready(function(){
-	console.log('I was here');
-	$('#products .flayout').on('hover', function(){
-		$(this).addClass('open');
-	});
+$(document).ready(function()
+{
+
+	links = $('.filterbar').find('.filter-data a');
+    $(links).each(function(i){
+        $(this).click(function(e){
+        	e.preventDefault();
+        	var filterValue = $(this).text();
+        	var filterParent= $(this).parents('.filter-data');
+        	var filterWrapper = $(this).parents('.filter-wrapper');
+        	var filterTrigger =  $(filterParent).prev('.filter-trigger');
+        	console.log(filterTrigger);
+        	$(filterTrigger).find('a').text("" + filterValue + "" );
+
+        	$(filterWrapper).next('.filter-wrapper').find('.filter-trigger').removeClass('not-active');
+
+
+        	//Activating the next filter
+
+        });
+    });
+
 
 });
