e09f6be2e2026853717fa77cfe572ffc8537a4b8
Elina <elina.sundukova@gmail.com>
2013-11-28 18:24:28 +0100

    language menu changed, footer, header

--- Klickdummy/assets/js/offcanvas-nav.js
+++ Klickdummy/assets/js/offcanvas-nav.js
@@ -1,3 +1,5 @@
+// OFF CANVAS NAVIGATION for javascript enabled
+
 $(document).ready(function() {
 
 	$('[data-toggle="slide-off"]').on('click', function(e) {
@@ -15,6 +17,19 @@ $(document).ready(function() {
 	$( window ).resize(function() {
 	 	$('.container').css('left', '0');
 	 	$('.navbar-collapse').removeClass('in').addClass('collapse');
+	});
+
+
+
+	$('footer').find('.dropdown-menu a').each(function(){
+		$(this).on('click', function(e){
+			var newClass= $(this).find('span').attr('class');
+
+			$(this).closest('.dropdown').find('.dropdown-toggle').find('i').attr('class', newClass);
+			e.preventDefault();
+
+
+		});
 	});
 
 });
